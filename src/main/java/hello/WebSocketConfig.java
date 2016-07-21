e12d15674bf578b685d8ade38772c164e92cd49e
danone771909 <danone771909@gmail.com>
2016-07-21 08:08:35 +0200

    New version - try implemented in your prooject

--- src/main/java/hello/WebSocketConfig.java
+++ src/main/java/hello/WebSocketConfig.java
@@ -5,24 +5,16 @@ import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.AbstractWebSocketMessageBrokerConfigurer;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
-
-/**
- * Created by daniel on 19.07.16.
- */
 @Configuration
 @EnableWebSocketMessageBroker
-public class WebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer{
-
-
+public class WebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer {
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
         config.enableSimpleBroker("/topic");
-        config.setApplicationDestinationPrefixes("/app");
+        config.setApplicationDestinationPrefixes("/calcApp");
     }
-
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/hello").withSockJS();
+        registry.addEndpoint("/add").withSockJS();
     }
-
 }
